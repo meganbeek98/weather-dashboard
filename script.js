e175fb823097466d15775742fd20cda820889d81
meganbeek98 <meganbeek98@gmail.com>
2022-06-25 03:33:43 -0400

    going a different route/ debugging

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
-
+var APIkey = '&appid=c70ed702f401fa0e4a0c11c4caa37f82';
 
 var input = document.querySelector('.input')
-var name = document.querySelector('.name')
-var description = document.querySelector('.description')
-var temp = document.querySelector('.temp')
-var button = document.querySelector('.button')
+var citiesList = document.querySelector('cities-list')
+var searchBtn = document.querySelector('.search-button')
 
-button.addEventListener('click',function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=c70ed702f401fa0e4a0c11c4caa37f82')
-    .then(response => response.json())
-    .then(data => {
-        var nameEl = data['name'];
-        var tempEl = data['main']['temp'];
-        var descriptionEl = data['weather'][0]['description'];
 
-        name.innerHTML = nameEl;
-        temp.innerHTML = tempEl;
-        description.innerHTML = descriptionEl;
+var cityName = localStorage.getItem('cityNameStore')
 
-    })
+// link for current day's weater info
+var weatherLink = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + '&units=imperial' + APIkey;
 
-.catch(err => alert("You entered an INVALID city name!"))
 
-})
+var forecastLink = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + '&units=imperial' + APIkey;
+
+
+// .catch(err => alert("You entered an INVALID city name!"))
 
 //     var tempValue/tempEl = data['main']['temp'];
 //     var nameValue/nameEl = data['name'];
