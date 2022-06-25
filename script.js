87dce50c98eed4bf4520060990a3020249ab2e6a
meganbeek98 <meganbeek98@gmail.com>
2022-06-25 01:12:28 -0400

    added function for button

--- script.js
+++ script.js
@@ -6,9 +6,14 @@ var name = document.querySelector('.name')
 var decscription = document.querySelector('.description')
 var temp = document.querySelector('.temp')
 
+button.addEventListener('click',function(){
+
+    
+})
 fetch ('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=c70ed702f401fa0e4a0c11c4caa37f82')
-.then(response => response.json())
-.then(data => {
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .then(data => {
     var tempValue = data['main']['temp'];
     var nameValue = data['name'];
     var descriptonValue = data['weather'][0]['description'];
