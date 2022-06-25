10696c4dfd70fdf18f0e8f67c1a9135eeecba736
meganbeek98 <meganbeek98@gmail.com>
2022-06-25 01:27:35 -0400

    created variables

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 const { response } = require("express")
 
 var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
+var inputEl = document.querySelector('.inputEl')
 var name = document.querySelector('.name')
 var decscription = document.querySelector('.description')
 var temp = document.querySelector('.temp')
 
 button.addEventListener('click',function(){
-
-    
-})
-fetch ('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=c70ed702f401fa0e4a0c11c4caa37f82')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=c70ed702f401fa0e4a0c11c4caa37f82')
     .then(response => response.json())
-    .then(data => console.log(data))
     .then(data => {
-    var tempValue = data['main']['temp'];
-    var nameValue = data['name'];
-    var descriptonValue = data['weather'][0]['description'];
+        var nameEl = data['name'];
+        var tempEl = data['temp'];
+        var decriptionEl = data['weather'][0]['description'];
+    })
+
+.catch(err => alert("You entered an INVALID city name!"))
 
 })
+})
+
+//     var tempValue/tempEl = data['main']['temp'];
+//     var nameValue/nameEl = data['name'];
+//     var descriptonValue/descriptionEl = data['weather'][0]['description'];
