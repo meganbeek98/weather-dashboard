797d3090679190cbb56d41053adfafd5d8862958
meganbeek98 <meganbeek98@gmail.com>
2022-06-25 01:38:58 -0400

    alerts when invalid city is entered

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const { response } = require("express")
 
-var button = document.querySelector('.button')
-var inputEl = document.querySelector('.inputEl')
+
+var input = document.querySelector('.input')
 var name = document.querySelector('.name')
-var decscription = document.querySelector('.description')
+var description = document.querySelector('.description')
 var temp = document.querySelector('.temp')
+var button = document.querySelector('.button')
 
 button.addEventListener('click',function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=c70ed702f401fa0e4a0c11c4caa37f82')
     .then(response => response.json())
     .then(data => {
         var nameEl = data['name'];
-        var tempEl = data['temp'];
+        var tempEl = data['main']['temp'];
         var decriptionEl = data['weather'][0]['description'];
+
+        name.innerHTML = nameEl;
+        temp.innerHTML = tempEl;
+        description.innerHTML = descriptionEl;
+
     })
 
 .catch(err => alert("You entered an INVALID city name!"))
 
-})
 })
 
 //     var tempValue/tempEl = data['main']['temp'];
